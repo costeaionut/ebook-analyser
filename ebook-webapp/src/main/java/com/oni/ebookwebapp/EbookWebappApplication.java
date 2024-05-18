c7013c3fdc51c704ce698be133015eb28f722d60
Costea Ionut-Andrei <46798810+costeaionut@users.noreply.github.com>
2024-05-18 23:47:32 +0300

    Initial commit (#1)

---
+++ ebook-webapp/src/main/java/com/oni/ebookwebapp/EbookWebappApplication.java
@@ -0,0 +1,13 @@
+package com.oni.ebookwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbookWebappApplication.class, args);
+    }
+
+}
---
+++ ebook-webapp/src/test/java/com/oni/ebookwebapp/EbookWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oni.ebookwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbookWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
